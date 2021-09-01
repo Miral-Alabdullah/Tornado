34d9026bf4f7f7391f937e880eb8724b8866ba59
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-09-02 00:35:48 +0300

    Almost finished

---
+++ app/src/main/java/com/example/tornado/DishAdapter.java
@@ -0,0 +1,63 @@
+package com.example.tornado;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+//Extends the Adapter class in order to indicate that it is an Adapter class
+public class DishAdapter extends RecyclerView.Adapter<DishAdapter.DishViewHolder>{
+    List<Dish> dishes;
+
+    public DishAdapter(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    //This class is to wrap the data and the view objects
+    public static class DishViewHolder extends RecyclerView.ViewHolder{
+
+        public Dish dish;
+        View itemView;
+
+        public DishViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+    @NotNull
+    @Override
+    public DishViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Create the view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_dish, parent, false);
+        return new DishViewHolder(view);
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull DishViewHolder holder, int position) {
+        holder.dish = dishes.get(position); //Each dish based on its position in the List
+
+        TextView dishName = holder.itemView.findViewById(R.id.dishNameTextView);
+        TextView dishPrice = holder.itemView.findViewById(R.id.dishPriceTextView);
+
+        dishName.setText(holder.dish.Name);
+        dishPrice.setText(Double.toString(holder.dish.Price));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dishes.size(); //return the list no matter how much its size
+    }
+
+}
--- app/src/main/java/com/example/tornado/Menu.java
+++ app/src/main/java/com/example/tornado/Menu.java
@@ -1,20 +1,39 @@
 package com.example.tornado;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu extends AppCompatActivity {
 
+    RecyclerView recyclerView;
     Button goToHomePage;
+    List<Dish> dishes = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
         goToHomePage = findViewById(R.id.gotToHomeBtn);
+
+        //Get The Data from the database
+        DishDatabase dishDatabase = DishDatabase.getTheInstance(this.getApplicationContext());
+        dishes = dishDatabase.dishDao().getAllDishes();
+
+
+        //To set the recyclerView we need to to set the adapter and for LayoutManager its optional,
+        // it indicates the way of displaying the items
+        recyclerView = findViewById(R.id.recyclerViewToListTheDishes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new DishAdapter(dishes));
     }
 
 
